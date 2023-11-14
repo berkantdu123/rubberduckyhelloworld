5d2c996f311fac9f74321fe13709fa8a60ceb5b5
berkantdu123 <52573108+berkantdu123@users.noreply.github.com>
2023-11-13 16:31:34 -0800

    Add files via upload

---
+++ New_Text_Document_4.bat
@@ -0,0 +1,38 @@
+@echo off
+cls
+echo 8888888b.           888      888                          8888888b.                    888               
+echo 888   Y88b          888      888                          888  "Y88b                   888               
+echo 888    888          888      888                          888    888                   888               
+echo 888   d88P 888  888 88888b.  88888b.   .d88b.  888d888    888    888 888  888  .d8888b 888  888 888  888 
+echo 8888888P"  888  888 888 "88b 888 "88b d8P  Y8b 888P"      888    888 888  888 d88P"    888 .88P 888  888 
+echo 888 T88b   888  888 888  888 888  888 88888888 888        888    888 888  888 888      888888K  888  888 
+echo 888  T88b  Y88b 888 888 d88P 888 d88P Y8b.     888        888  .d88P Y88b 888 Y88b.    888 "88b Y88b 888 
+echo 888   T88b  "Y88888 88888P"  88888P"   "Y8888  888        8888888P"   "Y88888  "Y8888P 888  888  "Y88888 
+echo                                                                                                      888 
+echo                                                                                                 Y8b d88P 
+echo                                                                                                  "Y88P"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+echo d8b   db  .d88b.  db   d8b   db      d888888b db   db d888888b .d8888.   d888888b .d8888.    .d88b.  d8b   db db      db    db   d888888b db   db d88888b   .d8888. d888888b  .d8b.  d8888b. d888888b      Cb     
+echo 888o  88 .8P  Y8. 88   I8I   88      `~~88~~' 88   88   `88'   88'  YP     `88'   88'  YP   .8P  Y8. 888o  88 88      `8b  d8'   `~~88~~' 88   88 88'       88'  YP `~~88~~' d8' `8b 88  `8D `~~88~~'   db  `8b   
+echo 88V8o 88 88    88 88   I8I   88         88    88ooo88    88    `8bo.        88    `8bo.     88    88 88V8o 88 88       `8bd8'       88    88ooo88 88ooooo   `8bo.      88    88ooo88 88oobY'    88      VP    8b  
+echo 88 V8o88 88    88 Y8   I8I   88         88    88~~~88    88      `Y8b.      88      `Y8b.   88    88 88 V8o88 88         88         88    88~~~88 88~~~~~     `Y8b.    88    88~~~88 88`8b      88            88D 
+echo 88  V888 `8b  d8' `8b d8'8b d8' db      88    88   88   .88.   db   8D     .88.   db   8D   `8b  d8' 88  V888 88booo.    88         88    88   88 88.       db   8D    88    88   88 88 `88.    88      db    8P  
+echo VP   V8P  `Y88P'   `8b8' `8d8'  V8      YP    YP   YP Y888888P `8888Y'   Y888888P `8888Y'    `Y88P'  VP   V8P Y88888P    YP         YP    YP   YP Y88888P   `8888Y'    YP    YP   YP 88   YD    YP      VP  .8P   
+echo                                  P                                                                                                                                                                         CP     
+:loop
+color 0
+color 1
+color 2
+color 3
+color 4
+color 5
+color 6
+color 7
+color 8
+color 9
+color A
+color B
+color C
+color D
+color E
+color F
+goto loop
